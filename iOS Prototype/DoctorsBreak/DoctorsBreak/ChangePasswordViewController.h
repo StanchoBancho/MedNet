d8c95b853d15f5dc11c5f29730b3337fe219b229
Pavlina Gatova <p.gatova@gmail.com>
2013-05-13 22:50:18 +0300

    tabbar with main VCs
    make Profile and Collegues VCs only in storyboard

---
+++ iOS Prototype/DoctorsBreak/DoctorsBreak/ChangePasswordViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChangePasswordViewController.h
+//  DoctorsBreak
+//
+//  Created by Pavlina Gatova on 5/13/13.
+//  Copyright (c) 2013 Stanimir Nikolov. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ChangePasswordViewController : NSViewController
+
+@end
---
+++ iOS Prototype/DoctorsBreak/DoctorsBreak/ChangePasswordViewController.m
@@ -0,0 +1,27 @@
+//
+//  ChangePasswordViewController.m
+//  DoctorsBreak
+//
+//  Created by Pavlina Gatova on 5/13/13.
+//  Copyright (c) 2013 Stanimir Nikolov. All rights reserved.
+//
+
+#import "ChangePasswordViewController.h"
+
+@interface ChangePasswordViewController ()
+
+@end
+
+@implementation ChangePasswordViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
