9ac7fc5560d809450536a517d9265e181ecfb258
StanchoBancho <nikolov.stanimir@gmail.com>
2013-06-01 23:00:44 +0300

    Added EGO pullto refresh table view for ios5.0 and above :)

--- iOS Prototype/DoctorsBreak/DoctorsBreak/NewsFeedViewController.h
+++ iOS Prototype/DoctorsBreak/DoctorsBreak/NewsFeedViewController.h
@@ -2,7 +2,7 @@
 //  NewsFeedViewController.h
 //  DoctorsBreak
 //
-//  Created by Stanimir Nikolov on 5/11/13.
+//  Created by Stanimir Nikolov on 6/1/13.
 //  Copyright (c) 2013 Stanimir Nikolov. All rights reserved.
 //
 
